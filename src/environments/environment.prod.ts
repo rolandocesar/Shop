eeae178a6e934d621d005f1677c4e728fb6a4325
Rolando Cesar <rolandocesar95@gmail.com>
2022-10-07 15:13:53 -0400

    ok

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // TODO: put baseUrl: ...
-  baseUrl: 'https://banao-shop.herokuapp.com/'
+  baseUrl: 'https://banao-shop.herokuapp.com/api'
 
 };
